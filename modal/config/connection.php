535ae8cda1872f66bfc15d6694d65c029c3716c1
DevGenevieve <umehgenevievechiamaka@gmail.com>
2025-08-13 16:10:49 +0100

    .env file configuration

---
+++ modal/config/connection.php
@@ -0,0 +1,26 @@
+<?php
+   
+   require_once('./config.php');
+
+   try{
+
+    loadEnv();
+
+    $hostName = getenv('DB_HOST');
+    $userName = getenv('DB_USER');
+    $password = getenv('DB_PASSWORD');
+    $dbName = getenv('DB_NAME');
+
+    $conn = mysqli_connect($hostName, $userName, $password, $dbName);
+
+    if(!$conn){
+        echo "Failed to connect to db:" . mysqli_connect_error();
+    }else{
+        echo "DB connection was successful";
+    }
+    
+   }catch(Exception $e){
+    die("Error loading environment variable: " . $e->getMessage());
+   }
+   
+?>
